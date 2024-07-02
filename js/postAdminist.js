7df98c49b240b6d86928f6a8ca6359fa8fda5b95
migonmir <110043575+migonmir@users.noreply.github.com>
2024-07-02 11:45:09 -0600

    Create postAdminist.js
    agrego el post

---
+++ js/postAdminist.js
@@ -0,0 +1,65 @@
+// cuando el dom se cargue
+document.addEventListener('DOMContentLoaded', async () => {
+    // tengo que levantar los datos del formulario, validar los inputs, en caso de algun value vacio, mostrar un mensaje de error
+    // y no hacer la peticion fetch
+    // si los datos son correctos, realizo la peticion fetch post a la api para agregar una pelicula
+    // si la respuesta es correcta, muestro un mensaje de exito y limpio los inputs del formulario
+    
+    //obtengo el formulario
+    formNuevaPelicula = document.getElementById('formGestion');
+    //agrego el evento submit al formulario
+    formNuevaPelicula.addEventListener('submit', async (event) => {
+        //prevengo el comportamiento por defecto del formulario
+        event.preventDefault();
+        //obtengo los datos del formulario
+        const formData = new FormData(formGestion);
+        //obtengo los valores de los inputs
+        const titulo = formData.get('titulo');
+        const genero = formData.get('genero');
+        const director = formData.get('director');
+        const duracion = formData.get('duracion');
+        const reparto = formData.get('reparto');
+        const sinopsis = formData.get('sinopsis');
+        const imagen = formData.get('imagen');
+        //valido los inputs
+        if (titulo === '' || genero === '' || duracion === '' || imagen === '') {
+            alert('Todos los campos son obligatorios');
+            return;
+        }
+        // levanto solo el nombre del file para enviarlo a la api
+        const imageName = imagen.name;
+      
+        //configuracion de options, es un post y necesita body
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                titulo: titulo,
+                genero: genero,
+                director: director,
+                duracion: duracion,
+                reparto: reparto,
+                sinopsis: sinopsis,
+                imagen: imagen
+            })
+        };
+        //realizo la peticion fetch a la api para agregar una pelicula
+        const response = await fetch('http://localhost:8080/app/peliculas', options);
+        //obtengo la respuesta
+        const data = await response.json();
+        //si la respuesta es correcta, muestro un mensaje de exito y limpio los inputs del formulario
+        // si el codigo es 201, la pelicula se agrego correctamente
+        if (response.status === 201) {
+            alert('Pelicula agregada correctamente');
+            formNuevaPelicula.reset();
+            // que se recargue la pagina para ver la pelicula agregada
+            location.reload();
+        } else {
+            alert('Error al agregar la pelicula');
+        }
+       
+    });
+
+});
